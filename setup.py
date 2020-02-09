6677df810c71d5d0ac3343fe6fe054f55410a1a3
jaycosaur <jaycorichter@gmail.com>
2020-02-09 17:23:54 +1100

    docs: update description

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="queue_utilities",
-    version="0.0.1",
+    version="0.0.2",
     author="Jacob Richter",
     author_email="jaycorichter@gmail.com",
-    description="Micropython compatible producer/subscriber event bus system with optional threading.",
+    description="A collection of helpful queue utilities. Pipes, timers, tickers, multiplexors, multicasters and queue select.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/jaycosaur/queue-utilities",
