ebba58c84f74f05c34f61dc1220836ccacb76a46
jaycosaur <jaycorichter@gmail.com>
2020-03-28 22:08:23 +1100

    feat: add types to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools  # type: ignore
 
 setuptools.setup(
     name="queue_utilities",
-    version="0.0.11",
+    version="0.0.12",
     author="Jacob Richter",
     author_email="jaycorichter@gmail.com",
     description="A collection of helpful queue utilities. Pipes, timers, tickers, multiplexors, multicasters and queue select.",
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jaycosaur/queue-utilities",
     packages=setuptools.find_packages(),
+    package_data={"queue_utilities": ["py.typed"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
